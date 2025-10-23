a3b2808d64085c712283be4a20cb2f01dbcedad3
cezugwu <cezugwu11@gmail.com>
2025-10-23 16:34:16 +0100

    random change

--- src/utilis/config.jsx
+++ src/utilis/config.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://127.0.0.1:8000";
-export const BASE_API_URL = "http://127.0.0.1:8000/api"
+export const BASE_URL = "https://ecom-6mu8.onrender.com";
+export const BASE_API_URL = "https://ecom-6mu8.onrender.com/api"
 export const BASE_IMAGE_URL = "https://res.cloudinary.com/djim0s6vb/"
